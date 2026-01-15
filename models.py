b38e454f022e4c915f0200c8c56f0c61ab4f4454
angel77rambo <anqiyang@UCHICAGO.edu>
2026-01-16 05:09:29 +0800

    Add models modula

--- models.py
+++ models.py
@@ -4,11 +4,18 @@ from abc import ABC, abstractmethod
 
 @dataclass(frozen=True)
 class MarketDataPoint:
+    """
+    Immutable market data structure.
+    """
     timestamp: datetime
     symbol: str
     price: float
-    
+
+
 class Strategy(ABC):
+    """
+    Abstract base class for trading strategies.
+    """
 
     @abstractmethod
     def generate_signals(self, tick: MarketDataPoint) -> list:
